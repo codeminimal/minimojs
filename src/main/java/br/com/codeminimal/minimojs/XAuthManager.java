562f891414250ac269132969f761a7880686ecc4
eduardo <eduardo.manrique@gmail.com>
2016-10-22 20:36:40 -0200

    first commit

---
+++ src/main/java/br/com/codeminimal/minimojs/XAuthManager.java
@@ -0,0 +1,89 @@
+package br.com.codeminimal.minimojs;
+
+import javax.servlet.ServletContext;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.*;
+
+/**
+ * Created by eduardo on 8/21/16.
+ */
+public enum XAuthManager {
+    instance;
+
+    private ServletContext ctx;
+
+    public void init(ServletContext ctx) {
+        this.ctx = ctx;
+    }
+
+    public AuthProperties getAuthProperties(String pathInfo) throws IOException {
+        int dotIndex = pathInfo.lastIndexOf('.');
+        int barIndex = pathInfo.lastIndexOf('/');
+        String propPath;
+        if (dotIndex > barIndex) {
+            propPath = pathInfo.substring(0, dotIndex);
+        } else {
+            propPath = pathInfo;
+        }
+        byte[] bytes = XFileUtil.instance.readFromDisk(
+                "/pages" + propPath + (propPath.lastIndexOf('/') == 0 ? "/index" : "") + ".auth", null,
+                this.ctx);
+        if (bytes == null) {
+            bytes = XFileUtil.instance.readFromDisk("/pages" + propPath.substring(0, propPath.lastIndexOf('/')) + "/auth",
+                    null, this.ctx);
+        }
+        AuthProperties result = null;
+        if (bytes != null) {
+            result = new AuthProperties();
+            StringReader reader = new StringReader(new String(bytes));
+            Properties p = new Properties();
+            p.load(reader);
+            String roles = (String) p.get("roles");
+            if (roles != null) {
+                String[] rolesArray = roles.split(",");
+                for (int i = 0; i < rolesArray.length; i++) {
+                    rolesArray[i] = rolesArray[i].trim();
+                }
+                result.setAllowedRoles(rolesArray);
+            }
+            String function = (String) p.get("function");
+            if (function != null) {
+                result.setAllowedFunction(function);
+            }
+            String authentication = (String) p.get("authentication");
+            result.setNeedsAuthentication(authentication == null || authentication.equalsIgnoreCase("TRUE"));
+        }
+        return result;
+    }
+
+    public static class AuthProperties {
+        private String[] allowedRoles;
+        private String allowedFunction;
+        private boolean needsAuthentication;
+
+        public String[] getAllowedRoles() {
+            return allowedRoles;
+        }
+
+        public void setAllowedRoles(String[] allowedRoles) {
+            this.allowedRoles = allowedRoles;
+        }
+
+        public String getAllowedFunction() {
+            return allowedFunction;
+        }
+
+        public void setAllowedFunction(String allowedFunction) {
+            this.allowedFunction = allowedFunction;
+        }
+
+        public Boolean getNeedsAuthentication() {
+            return needsAuthentication;
+        }
+
+        public void setNeedsAuthentication(Boolean needsAuthentication) {
+            this.needsAuthentication = needsAuthentication;
+        }
+    }
+}
---
+++ src/main/java/br/com/codeminimal/minimojs/XFileUtil.java
@@ -0,0 +1,122 @@
+package br.com.codeminimal.minimojs;
+
+import org.apache.commons.io.IOUtils;
+
+import javax.servlet.ServletContext;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public enum XFileUtil {
+
+    instance;
+
+    private final String[] IMG_EXTENSIONS = {".jpg", ".jpeg", ".png", ".gif"};
+
+    private byte[] gif;
+
+    XFileUtil() {
+        InputStream in = XFileUtil.class.getResourceAsStream("/px.gif");
+        try {
+            gif = IOUtils.toByteArray(in);
+        } catch (IOException e) {
+            throw new RuntimeException("Erro carregando a imagem vazia!", e);
+        }
+
+    }
+
+    public boolean validateImage(String fileName) {
+
+        for (String extension : IMG_EXTENSIONS) {
+            if (fileName.toUpperCase().endsWith(extension.toUpperCase()))
+                return true;
+        }
+
+        return false;
+    }
+
+    public void printUploadResponse(boolean ok) throws IOException {
+
+        OutputStream out = XContext.getXResponse().getOutputStream();
+        out.write(("<html><script>parent.X._uploadResponse('" + ok + "');</script></html>").getBytes());
+        out.flush();
+    }
+
+    public String getResource(String path) throws IOException {
+        return XStreamUtil.inputStreamToString(XServlet.class.getResourceAsStream(path));
+    }
+
+    public void printEmptyGif() throws IOException {
+        XContext.getXResponse().setContentType("image/gif");
+        OutputStream out = XContext.getXResponse().getOutputStream();
+        out.write(gif);
+        out.flush();
+    }
+
+    public void sendFile(XFile f) throws IOException {
+        XContext.getXResponse().setContentType(f.getContentType());
+        XContext.getXResponse().setContentLength(f.getData().length);
+        OutputStream output = XContext.getXResponse().getOutputStream();
+        output.write(f.getData());
+        output.flush();
+    }
+
+    public String readFile(String fileName) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        try {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append("\n");
+                line = br.readLine();
+            }
+            return sb.toString();
+        } finally {
+            br.close();
+        }
+    }
+
+    public byte[] readFromDisk(String path, String defaultPath, ServletContext ctx) throws IOException {
+        path = path.replaceAll("//", "/");
+        String diskPath = ctx.getRealPath(path);
+        if (diskPath != null) {
+            InputStream is;
+            File file = new File(diskPath);
+            if (file.exists()) {
+                is = new FileInputStream(file);
+            } else {
+                is = ctx.getResourceAsStream(path);
+                if (is == null && defaultPath != null) {
+                    is = XServlet.class.getResourceAsStream(defaultPath);
+                }
+            }
+            return is != null ? XStreamUtil.inputStreamToByteArray(is) : null;
+        }
+        return null;
+    }
+
+    public List<File> listFiles(String folderPath, FilenameFilter filter, ServletContext ctx) throws IOException {
+        List<File> result = new ArrayList<File>();
+        folderPath = folderPath.replaceAll("//", "/");
+        String diskPath = ctx.getRealPath(folderPath);
+        if (diskPath != null) {
+            File folder = new File(diskPath);
+            findFiles(folder, filter, result);
+        }
+        return result;
+    }
+
+    private void findFiles(File folder, FilenameFilter filter, List<File> result) {
+        File[] files = folder.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            File file = files[i];
+            if (file.isDirectory()) {
+                findFiles(file, filter, result);
+            } else if (filter.accept(folder, file.getName())) {
+                result.add(file);
+            }
+        }
+    }
+}
